28c67b0eec8a2b45f9ba920771674d36695c37d9
rllola <rllola80@gmail.com>
2022-05-13 20:42:45 +0200

    working using wat2wasm

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -5,10 +5,29 @@ use fvm_ipld_blockstore::MemoryBlockstore;
 use fvm_ipld_encoding::tuple::*;
 use fvm_shared::address::Address;
 use fvm_shared::bigint::BigInt;
+use wabt::wat2wasm;
 use std::env;
 
 const WASM_COMPILED_PATH: &str =
     "../build/debug.wasm";
+
+const WAT: &str = r#"
+;; Mock invoke function
+(module
+    (type $i32_=>_i32 (func (param i32) (result i32)))
+    (type $none_=>_none (func))
+    (memory $0 1)
+    (export "invoke" (func $assembly/index/invoke))
+    (export "constructor" (func $assembly/index/constructor))
+    (export "memory" (memory $0))
+    (func $assembly/index/invoke (param $0 i32) (result i32)
+     i32.const 0
+    )
+    (func $assembly/index/constructor
+     nop
+    )
+   )
+"#;
 
 /// The state object.
 #[derive(Serialize_tuple, Deserialize_tuple, Clone, Debug, Default)]
@@ -17,7 +36,7 @@ pub struct State {
 }
 
 fn main() {
-    println!("Testing Hello Worls contract in assembly script");
+    println!("Testing Hello World contract in assembly script");
 
     let mut tester = Tester::new(
         NetworkVersion::V15,
@@ -28,12 +47,14 @@ fn main() {
 
     let sender: [Account; 1] = tester.create_accounts().unwrap();
 
-    let wasm_path = env::current_dir()
+    /*let wasm_path = env::current_dir()
     .unwrap()
     .join(WASM_COMPILED_PATH)
     .canonicalize()
     .unwrap();
-    let wasm_bin = std::fs::read(wasm_path).expect("Unable to read file");
+    let wasm_bin = std::fs::read(wasm_path).expect("Unable to read file");*/
+
+    let wasm_bin = wat2wasm(WAT).unwrap();
 
     let actor_state = State::default();
     let state_cid = tester.set_state(&actor_state).unwrap();
