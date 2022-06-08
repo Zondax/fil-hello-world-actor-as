9612ec7aadfcf271c6e4ff2b900a28f39792d9a1
rllola <rllola80@gmail.com>
2022-06-08 14:37:26 +0200

    fix integration

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -4,10 +4,8 @@ use fvm_shared::state::StateTreeVersion;
 use fvm_shared::version::NetworkVersion;
 use fvm::executor::{ApplyKind, Executor};
 use fvm_ipld_blockstore::MemoryBlockstore;
-use fvm::{init_actor};
 use fvm_ipld_blockstore::Blockstore;
 use fvm_ipld_encoding::tuple::*;
-use fvm_ipld_encoding::RawBytes;
 use fvm_shared::address::Address;
 use fvm_shared::bigint::BigInt;
 use std::env;
@@ -33,8 +31,6 @@ fn main() {
 
     let sender: [Account; 1] = tester.create_accounts().unwrap();
 
-    dbg!(sender[0].1.to_string());
-
     let wasm_path = env::current_dir()
     .unwrap()
     .join(WASM_COMPILED_PATH)
@@ -53,9 +49,8 @@ fn main() {
         .unwrap();
 
     let actor = tester.state_tree.as_ref().unwrap().get_actor(&actor_address).unwrap().unwrap();
-    dbg!(&actor.state.to_string());
-    /*let state = tester.blockstore().clone().get(&actor_state.state).unwrap();
-    dbg!(state);*/
+    let state = tester.blockstore().clone().get(&actor.state).unwrap();
+    println!("Cbor hex state : {}", hex::encode(state.unwrap()));
 
     // Instantiate machine
     tester.instantiate_machine().unwrap();
@@ -65,20 +60,28 @@ fn main() {
         constructor_params: RawBytes::new(vec![])
     };
 
-    dbg!(hex::encode(&*RawBytes::serialize(params).unwrap()));*/
+    dbg!(hex::encode(&*RawBytes::serialize(params).unwrap()));
 
-    println!("Calling construct");
+    println!("Calling `construct`");
     let message = Message {
         from: sender[0].1,
         to: init_actor::INIT_ACTOR_ADDR,
         gas_limit: 1000000000,
         method_num: 2,
-        params: RawBytes::new(hex::decode("82d82a5827000155a0e4022002646fb340f5c4ef9e5846a589a04bef4e1abfb79601a7074352f3ba3bae22ed40").unwrap()),
+        params: RawBytes::new(hex::decode("82d82a5827000155a0e402203eb13f4db64778bbdabf7ded1e7c14ee0826e842570fddab85e3d24ede54109f4").unwrap()),
         // 82d82a5827000155a0e40220d06c3cd58386c52ad72b4d154e8c82b7c77580a0817d64099a1c44ca58b363aa40
         //params: RawBytes::new(hex::decode("82d82a5827000155a0e40220d06c3cd58386c52ad72b4d154e8c82b7c77580a0817d64099a1c44ca58b363aa40").unwrap()),
         ..Message::default()
-    };
+    };*/
 
+    println!("Calling `say_hello`");
+    let message = Message {
+        from: sender[0].1,
+        to: actor_address,
+        gas_limit: 1000000000,
+        method_num: 2,
+        ..Message::default()
+    };
 
     let res = tester
     .executor
@@ -90,9 +93,9 @@ fn main() {
 
     assert_eq!(res.msg_receipt.exit_code.value(), 0);
 
-    /*let actor_state_bis = tester.state_tree.get_actor(&actor_address).unwrap().unwrap();
+    /*let actor_state_bis = tester.state_tree.as_ref().unwrap().get_actor(&actor_address).unwrap().unwrap();
     dbg!(&actor_state_bis.state.to_string());
 
-    let state_bis = tester.state_tree.store().get(&actor_state_bis.state).unwrap();
+    let state_bis = tester.state_tree.as_ref().unwrap().store().get(&actor_state_bis.state).unwrap();
     dbg!(&state_bis);*/
 }
