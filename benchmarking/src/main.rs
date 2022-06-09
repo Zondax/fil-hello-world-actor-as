57ab68d22e767032d718d38c02caa5637ed42509
rllola <rllola80@gmail.com>
2022-06-09 22:57:45 +0200

    added build for assemblyscript

--- benchmarking/src/main.rs
+++ benchmarking/src/main.rs
@@ -9,9 +9,20 @@ use fvm_ipld_encoding::tuple::*;
 use fvm_shared::address::Address;
 use fvm_shared::bigint::BigInt;
 use std::env;
+use fvm::executor::ApplyRet;
 
 const RUST_WASM_COMPILED_PATH: &str =
     "./rust-hello-world-actor.wasm";
+
+const AS_INCREMENTAL_WASM_COMPILED_PATH: &str =
+    "./as-incremental-hello-world-actor.wasm";
+
+const AS_MINIMAL_WASM_COMPILED_PATH: &str =
+    "./as-minimal-hello-world-actor.wasm";
+
+const AS_STUB_WASM_COMPILED_PATH: &str =
+    "./as-stub-hello-world-actor.wasm";
+
 
 /// The state object.
 #[derive(Serialize_tuple, Deserialize_tuple, Clone, Debug, Default)]
@@ -19,9 +30,7 @@ pub struct State {
     pub count: u64,
 }
 
-fn main() {
-    println!("Running benchmark");
-
+fn run_fvm(path: &str) -> ApplyRet {
     let mut tester = Tester::new(
         NetworkVersion::V16,
         StateTreeVersion::V4,
@@ -33,7 +42,7 @@ fn main() {
 
     let wasm_path = env::current_dir()
     .unwrap()
-    .join(RUST_WASM_COMPILED_PATH)
+    .join(path)
     .canonicalize()
     .unwrap();
     let wasm_bin = std::fs::read(wasm_path).expect("Unable to read file");
@@ -51,7 +60,6 @@ fn main() {
     // Instantiate machine
     tester.instantiate_machine().unwrap();
 
-    println!("Calling `say_hello`");
     let message = Message {
         from: sender[0].1,
         to: actor_address,
@@ -66,5 +74,25 @@ fn main() {
     .execute_message(message, ApplyKind::Explicit, 100)
     .unwrap();
 
-    dbg!(&res);
+    return res
+}
+
+fn main() {
+    println!("Running benchmark");
+
+    let res_rust = run_fvm(RUST_WASM_COMPILED_PATH);
+
+    println!("Rust gas used : {}", &res_rust.msg_receipt.gas_used);
+
+    let res_as_incremental = run_fvm(AS_INCREMENTAL_WASM_COMPILED_PATH);
+
+    println!("Assemblyscript (incremental) gas used : {}", res_as_incremental.msg_receipt.gas_used);
+
+    let res_as_minimal = run_fvm(AS_MINIMAL_WASM_COMPILED_PATH);
+
+    println!("Assemblyscript (minimal) gas used : {}", res_as_minimal.msg_receipt.gas_used);
+
+    let res_as_stub = run_fvm(AS_STUB_WASM_COMPILED_PATH);
+
+    println!("Assemblyscript (stub) gas used : {}", res_as_stub.msg_receipt.gas_used);
 }
