6d95a795166cf04cc40350994f72eaf129444b35
rllola <rllola80@gmail.com>
2022-06-13 15:17:34 +0200

    Added go sdk to benchmark; show difference with minimal value;

--- benchmarking/src/main.rs
+++ benchmarking/src/main.rs
@@ -16,17 +16,19 @@ use std::fs;
 use prettytable::Table;
 
 const RUST_WASM_COMPILED_PATH: &str =
-    "./rust-hello-world-actor.wasm";
+    "./temp/rust-hello-world-actor.wasm";
 
 const AS_INCREMENTAL_WASM_COMPILED_PATH: &str =
-    "./as-incremental-hello-world-actor.wasm";
+    "./temp/as-incremental-hello-world-actor.wasm";
 
 const AS_MINIMAL_WASM_COMPILED_PATH: &str =
-    "./as-minimal-hello-world-actor.wasm";
+    "./temp/as-minimal-hello-world-actor.wasm";
 
 const AS_STUB_WASM_COMPILED_PATH: &str =
-    "./as-stub-hello-world-actor.wasm";
+    "./temp/as-stub-hello-world-actor.wasm";
 
+const GO_WASM_COMPILED_PATH: &str =
+    "./temp/go-hello-world-actor.wasm";
 
 /// The state object.
 #[derive(Serialize_tuple, Deserialize_tuple, Clone, Debug, Default)]
@@ -84,28 +86,49 @@ fn run_fvm(path: &str) -> ApplyRet {
 fn main() {
     println!("Running benchmark");
 
-
     let res_rust = run_fvm(RUST_WASM_COMPILED_PATH);
-    let rust_size = fs::metadata(RUST_WASM_COMPILED_PATH).unwrap();
-
+    let rust_size = fs::metadata(RUST_WASM_COMPILED_PATH).unwrap().len();
 
     let res_as_incremental = run_fvm(AS_INCREMENTAL_WASM_COMPILED_PATH);
-    let as_incremental_size = fs::metadata(AS_INCREMENTAL_WASM_COMPILED_PATH).unwrap();
-
+    let as_incremental_size = fs::metadata(AS_INCREMENTAL_WASM_COMPILED_PATH).unwrap().len();
 
     let res_as_minimal = run_fvm(AS_MINIMAL_WASM_COMPILED_PATH);
-    let as_minimal_size = fs::metadata(AS_MINIMAL_WASM_COMPILED_PATH).unwrap();
-
+    let as_minimal_size = fs::metadata(AS_MINIMAL_WASM_COMPILED_PATH).unwrap().len();
 
     let res_as_stub = run_fvm(AS_STUB_WASM_COMPILED_PATH);
-    let as_stub_size = fs::metadata(AS_STUB_WASM_COMPILED_PATH).unwrap();
+    let as_stub_size = fs::metadata(AS_STUB_WASM_COMPILED_PATH).unwrap().len();
+
+    let res_go = run_fvm(GO_WASM_COMPILED_PATH);
+    let go_size = fs::metadata(GO_WASM_COMPILED_PATH).unwrap().len();
+
+    let gas_used_array = vec![res_rust.msg_receipt.gas_used, res_as_incremental.msg_receipt.gas_used, res_as_minimal.msg_receipt.gas_used, res_go.msg_receipt.gas_used];
+    let min_gas_used = gas_used_array.iter().min().unwrap();
+
+    let file_size_array = vec![rust_size, as_incremental_size, as_minimal_size, as_stub_size, go_size];
+    let min_file_size = file_size_array.iter().min().unwrap();
 
     let mut table = Table::new();
     table.add_row(row!["ACTOR FILE", "GAS USED (say_hello())", "FILE SIZE"]);
-    table.add_row(row!["Rust actor", res_rust.msg_receipt.gas_used, rust_size.len()]);
-    table.add_row(row!["Assemblyscript (incremental)", res_as_incremental.msg_receipt.gas_used, as_incremental_size.len()]);
-    table.add_row(row!["Assemblyscript (minimal)", res_as_minimal.msg_receipt.gas_used, as_minimal_size.len()]);
-    table.add_row(row!["Assemblyscript (stub)", res_as_stub.msg_receipt.gas_used, as_stub_size.len()]);
+    table.add_row(row!["Rust actor",
+        format!("{} (+{})", res_rust.msg_receipt.gas_used, res_rust.msg_receipt.gas_used-min_gas_used),
+        format!("{} (+{})", rust_size, rust_size-min_file_size)],
+    );
+    table.add_row(row!["Assemblyscript (incremental)",
+        format!("{} (+{})", res_as_incremental.msg_receipt.gas_used, res_as_incremental.msg_receipt.gas_used-min_gas_used),
+        format!("{} (+{})", as_incremental_size, as_incremental_size-min_file_size)],
+        );
+    table.add_row(row!["Assemblyscript (minimal)",
+        format!("{} (+{})", res_as_minimal.msg_receipt.gas_used, res_as_minimal.msg_receipt.gas_used-min_gas_used),
+        format!("{} (+{})", as_minimal_size, as_minimal_size-min_file_size)],
+    );
+    table.add_row(row!["Assemblyscript (stub)",
+        format!("{} (+{})", res_as_stub.msg_receipt.gas_used, res_as_stub.msg_receipt.gas_used-min_gas_used),
+        format!("{} (+{})", as_stub_size,  as_stub_size-min_file_size)],
+    );
+    table.add_row(row!["Go actor",
+        format!("{} (+{})", res_go.msg_receipt.gas_used, res_go.msg_receipt.gas_used-min_gas_used),
+        format!("{} (+{})",go_size, go_size-min_file_size)],
+    );
 
     table.printstd();
 }
