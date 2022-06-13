9ac92f9c8bb76cec32c071ca4040eb339ceda39d
Emmanuel <emmanuelm41@gmail.com>
2022-06-13 16:01:49 -0300

    Make use of new basestate class to remove ipld logic (#10)
    * make use of new basestate class to remove ipld logic

--- assembly/state.ts
+++ assembly/state.ts
@@ -1,54 +1,33 @@
-import {setRoot} from "@zondax/fvm-as-sdk/assembly/wrappers";
-import {Put, Get, root} from "@zondax/fvm-as-sdk/assembly/helpers";
-import {Cid, DAG_CBOR} from "@zondax/fvm-as-sdk/assembly/env";
-import {CBOREncoder, CBORDecoder} from "@zondax/assemblyscript-cbor/assembly";
-import {Obj, Integer} from "@zondax/assemblyscript-cbor/assembly/types";
+import {BaseState} from "@zondax/fvm-as-sdk/assembly/utils/state";
+import {Value, Obj, Integer} from "@zondax/assemblyscript-cbor/assembly/types";
+import {CBOREncoder} from "@zondax/assemblyscript-cbor/assembly";
 
 // This class represents the actor state.
-// On save and load functions, persistence can be implemented.
-// Any value we can keep between calls should be written to the storage (IPLD)
-export class State {
+// The BaseState is an abstract class. It has IPLD logic to read and write data to storage
+// Any state class only needs to extend it, and implement encode, parse and load functions
+export class State extends BaseState{
     // In this case, we only keep a counter
     count:u64
 
     // Set initial value when instantiating a new state
     constructor(count: u64) {
+        super();
         this.count = count;
     }
 
-    // Function responsible to serialize state and save it to IPLD
-    save(): Cid{
+    // This function should only indicate how to serialize the store into CBOR
+    protected encode(): ArrayBuffer {
         // Use CBOREncoder to serialize data into CBOR
         const encoder = new CBOREncoder();
         encoder.addObject(1)
         encoder.addKey("counter")
         encoder.addUint64(this.count)
 
-        // Create a new block on IPLD with serialized data
-        // It returns the id of that new block
-        const encodedData = Uint8Array.wrap(encoder.serialize())
-        const stCid = Put(0xb220, 32, DAG_CBOR, encodedData)
-
-        // setRoot allows to attach that new block to the actor instance that is running
-        // If this is not done, the block won't be related to this actor, and it won't be able
-        // to access it
-        setRoot(stCid)
-
-        return stCid
+        return encoder.serialize()
     }
 
-    // Static function used to load the state attached or related to this actor instance
-    static load(): State{
-        // Get the block id related to this instance
-        const readCid = root()
-
-        // Using that id, get the block and read the serialized data
-        const ipldData = Get(readCid)
-
-        // Use CBORDecoder to CBOR data into an object
-        const decoder = new CBORDecoder(ipldData.buffer)
-        const rawState = decoder.parse()
-
+    // This function should only indicate how to convert from a generic object model to this state class
+    protected parse(rawState: Value): State {
         let counter:i64 = 0
         if(rawState.isObj){
             // Here we cast as object as we know that is what we saved before
@@ -60,5 +39,9 @@ export class State {
         }
 
         return new State(counter)
+    }
+
+    static load():State{
+        return new State(0).load() as State
     }
 }
