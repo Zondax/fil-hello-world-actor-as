327063407ff3c4246f029e4be70eabe4065000cf
Emmanuel <emmanuelm41@gmail.com>
2022-06-16 15:44:18 -0300

    remove invoke function and change it for decorators

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,22 +3,8 @@ import {create} from "@zondax/fvm-as-sdk/assembly/wrappers";
 import {DAG_CBOR} from "@zondax/fvm-as-sdk/assembly/env";
 import {State} from "./state";
 
-function mappingMethods(methodNum:u32, paramsID:u32):i32{
-  switch (methodNum){
-    case 2:
-      // Execute whatever the smart contract wants.
-      const msg = say_hello()
-
-      // If we want to return something as execution result,
-      // we need to create a block with those values, and return
-      // the output of that function
-      return create(DAG_CBOR, msg)
-    default:
-      return -1
-  }
-}
-
-// Function executed on create actor
+// @ts-ignore
+@constructor
 function init(paramsID:u32): void {
   // If we want to attach some storage to the instance,
   // a state needs to be saved at this step.
@@ -26,8 +12,10 @@ function init(paramsID:u32): void {
   new State(0).save()
 }
 
+// @ts-ignore
+@filecoinmethod
 // User function. Smart-contract-related function.
-function say_hello(): Uint8Array {
+function say_hello(paramsID:u32): u32 {
   // If we want to restore the storage related to this instance,
   // we should call static load function. It will return a preloaded
   // state
@@ -44,5 +32,7 @@ function say_hello(): Uint8Array {
 
   // On AssemblyScript, UTF16 is used. As filecoin uses UTF8, some encoding is required
   // Now we can return whatever we want
-  return  Uint8Array.wrap(String.UTF8.encode(message))
+  const msg = Uint8Array.wrap(String.UTF8.encode(message))
+
+  return create(DAG_CBOR, msg)
 }
