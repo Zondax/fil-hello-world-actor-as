789e2fb9723b8463c985d1c7cb4b5ad9ac85b9a4
Emmanuel <emmanuelm41@gmail.com>
2022-06-17 10:09:43 -0300

    Make use of a custom transformation to simplify contract (#14)
    * make use of a custom transformation to simplify contract
    * receive params id on method functions
    * remove invoke function and change it for decorators
    * change decorator name and add method index
    * remove last fvm related code
    * bump bindgen package

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,56 +1,19 @@
-import {methodNumber, usrUnhandledMsg, create} from "@zondax/fvm-as-sdk/assembly/wrappers";
-import {NO_DATA_BLOCK_ID, DAG_CBOR} from "@zondax/fvm-as-sdk/assembly/env";
-import {isConstructorCaller} from "@zondax/fvm-as-sdk/assembly/helpers";
+// @filecoinfile
 import {State} from "./state";
 
-// Entrypoint for filecoin smart contracts
-export function invoke(_: u32): u32 {
-
-  // Read invoked method number
-  const methodNum = methodNumber()
-
-  switch (u32(methodNum)) {
-    // Method number 1 is fixe for create actor command
-    case 1:
-      // Call constructor func.
-      constructor()
-      break
-
-    // Any other method is defined by the user
-    case 2:
-      // Execute whatever the smart contract wants.
-      const msg = say_hello()
-
-      // If we want to return something as execution result,
-      // we need to create a block with those values, and return
-      // the output of that function
-      return create(DAG_CBOR, msg)
-
-    // If the method number is not implemented, an error should be retrieved
-    default:
-      usrUnhandledMsg()
-  }
-
-  return NO_DATA_BLOCK_ID
-}
-
-// Function executed on create actor
-function constructor(): void {
-  // The caller of this method should be always the same.
-  // Nobody else should call the constructor
-  if( !isConstructorCaller() ) return;
-
+// @ts-ignore
+@constructor
+function init(paramsID:u32): void {
   // If we want to attach some storage to the instance,
   // a state needs to be saved at this step.
   // So we create a store, and call save method
   new State(0).save()
-
-  // No need to return anything
-  return;
 }
 
+// @ts-ignore
+@export_method(2)
 // User function. Smart-contract-related function.
-function say_hello(): Uint8Array {
+function say_hello(paramsID:u32): Uint8Array {
   // If we want to restore the storage related to this instance,
   // we should call static load function. It will return a preloaded
   // state
@@ -66,9 +29,8 @@ function say_hello(): Uint8Array {
   const message = "Hello world " + state.count.toString()
 
   // On AssemblyScript, UTF16 is used. As filecoin uses UTF8, some encoding is required
-  const msgBuff = String.UTF8.encode(message)
-  const buff = Uint8Array.wrap(msgBuff)
+  // Now we can return whatever we want
+  const msg = Uint8Array.wrap(String.UTF8.encode(message))
 
-  // Now re can return whatever we want
-  return buff
+  return msg
 }
