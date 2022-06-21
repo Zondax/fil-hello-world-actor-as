e7fdea4b0fcbd32504dba0e9ce46e3bcc394aa54
Emmanuel <emmanuelm41@gmail.com>
2022-06-21 16:10:53 -0300

    add  test for invoke method number 2

--- tests/api/index.test.ts
+++ tests/api/index.test.ts
@@ -4,7 +4,7 @@ import log4js from "log4js"
 import path from "path"
 import * as cbor from '@ipld/dag-cbor'
 
-jest.setTimeout(60000)
+jest.setTimeout(300 * 1000)
 
 const URL = process.env["NODE_URL"]
 const TOKEN = process.env["NODE_TOKEN"]
@@ -191,91 +191,72 @@ test("Create actor", async () => {
 })
 
 test("Invoke method 2", async () => {
-    if(!instanceAddress) return
+    for( let callNum = 1; callNum < 10; callNum++) {
+        if (!instanceAddress) return
 
-    logger.info(`Invoking method 2 from instance [${instanceAddress.toString()}]`)
+        logger.info(`Invoking method 2 from instance [${instanceAddress.toString()}] - Call N: [${callNum}]`)
 
-    const params = cbor.encode([ ])
-    logger.trace( "Params encoded")
+        const params = cbor.encode([])
+        logger.trace("Params encoded")
 
-    const seed = SEED || generateMnemonic()
-    logger.trace( `Seed: [${seed}]`)
+        const seed = SEED || generateMnemonic()
+        logger.trace(`Seed: [${seed}]`)
 
-    const keys = keyDerive(seed, "m/44'/461'/0/0/1", "")
-    logger.trace( `Address: ${keys.address}`)
+        const keys = keyDerive(seed, "m/44'/461'/0/0/1", "")
+        logger.trace(`Address: ${keys.address}`)
 
-    const filRPC = new FilecoinRPC({ url: URL, token: TOKEN })
-    const nonce = (await filRPC.getNonce(keys.address)).result
-    logger.trace( `Nonce: ${nonce}`)
-    /*
-    let tx = {
-        From:   keys.address,
-        To: INIT_ACTOR_ADDRESS,
-        Value:  "0",
-        Method: 2,
-        Params: Buffer.from(params).toString('base64'),
-        Nonce: nonce,
-        GasFeeCap: "0",
-        GasPremium: "0",
-        GasLimit: 0
-    }
+        const filRPC = new FilecoinRPC({url: URL, token: TOKEN})
+        const nonce = (await filRPC.getNonce(keys.address)).result
+        logger.trace(`Nonce: ${nonce}`)
 
-    try {
-        const fees = (await filRPC.getGasEstimation({...tx}))
-        logger.trace(`Fees: ${JSON.stringify(fees)}`)
-
-        expect(fees.result).toBeDefined()
-        expect(fees.result.GasFeeCap).toBeDefined()
-        expect(fees.result.GasPremium).toBeDefined()
-        expect(fees.result.GasLimit).toBeDefined()
-
-        const { GasFeeCap, GasPremium, GasLimit } = fees.result
-        tx = {
-            ...tx,
-            GasFeeCap,
-            GasPremium,
-            GasLimit
+        let tx = {
+            From: keys.address,
+            To: instanceAddress,
+            Value: "0",
+            Method: 2,
+            Params: Buffer.from(params).toString('base64'),
+            Nonce: nonce,
+            GasFeeCap: "0",
+            GasPremium: "0",
+            GasLimit: 0
         }
-    }catch(err){
-        logger.error(`Error fetching fees: ${JSON.stringify(err.response.data)}`)
-        expect(err).not.toBeDefined()
-        return
-    }
 
-    const signedTx = transactionSign(tx, keys.private_base64 )
+        try {
+            const fees = (await filRPC.getGasEstimation({...tx}))
+            logger.trace(`Fees: ${JSON.stringify(fees)}`)
 
-    const sentTx = await filRPC.sendSignedMessage({Message: tx, Signature: signedTx.Signature} )
-    logger.trace( `Sent tx response: ${JSON.stringify(sentTx)}`)
+            expect(fees.result).toBeDefined()
+            expect(fees.result.GasFeeCap).toBeDefined()
+            expect(fees.result.GasPremium).toBeDefined()
+            expect(fees.result.GasLimit).toBeDefined()
 
-    expect(sentTx.result).toBeDefined()
-    expect(sentTx.result.Receipt).toBeDefined()
-    expect(sentTx.result.Receipt.ExitCode).toBe(0)
+            const {GasFeeCap, GasPremium, GasLimit} = fees.result
+            tx = {
+                ...tx,
+                GasFeeCap,
+                GasPremium,
+                GasLimit
+            }
+        } catch (err) {
+            logger.error(`Error fetching fees: ${JSON.stringify(err.response.data)}`)
+            expect(err).not.toBeDefined()
+            return
+        }
 
-    if(sentTx.result.Receipt.ExitCode == 0){
+        const signedTx = transactionSign(tx, keys.private_base64)
 
-        let idAddr, robustAddr;
-        if( sentTx.result.ReturnDec){
-            idAddr = sentTx.result.ReturnDec.IDAddress
-            robustAddr = sentTx.result.ReturnDec.RobustAddress
-        } else {
+        const sentTx = await filRPC.sendSignedMessage({Message: tx, Signature: signedTx.Signature})
+        logger.trace(`Sent tx response: ${JSON.stringify(sentTx)}`)
+
+        expect(sentTx.result).toBeDefined()
+        expect(sentTx.result.Receipt).toBeDefined()
+        expect(sentTx.result.Receipt.ExitCode).toBe(0)
+
+        if (sentTx.result.Receipt.ExitCode == 0) {
             const respBuffer = Buffer.from(sentTx.result.Receipt.Return, "base64")
 
-            let arrayBuffer = new ArrayBuffer(respBuffer.length);
-            let typedArray = new Uint8Array(arrayBuffer);
-            for (let i = 0; i < respBuffer.length; ++i) {
-                typedArray[i] = respBuffer[i];
-            }
-
-            ([idAddr, robustAddr] = cbor.decode(typedArray))
+            logger.info(`Message: [${respBuffer.toString()}]`)
+            expect(respBuffer.toString()).toBe(`Hello world ${callNum}`)
         }
-
-        expect(idAddr).toBeDefined()
-        expect(robustAddr).toBeDefined()
-
-        logger.info(`ID Address: ${idAddr.toString()}`)
-        logger.info(`Robust address: ${robustAddr.toString()}`)
-
-        instanceAddres = idAddr.toString()
     }
-    */
 })
