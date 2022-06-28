c751d14ddff03f743b7260e5e85daf97272407f6
Emmanuel <emmanuelm41@gmail.com>
2022-06-28 10:22:07 -0300

    fix: add missing imports to package.json (#21)
    * add missing imports to package.json
    * add job to build browser test case
    * install make
    * fix ts issues to allow build app
    * change test name
    
    Co-authored-by: Prateek Rathod <prateek@zondax.ch>

--- tests/browser/pages/api/install.ts
+++ tests/browser/pages/api/install.ts
@@ -5,37 +5,46 @@ import cors from "cors";
 import * as cbor from "@ipld/dag-cbor";
 
 // @ts-ignore
-import {install} from "../../lib/index";
+import { install } from "../../lib/index";
+
+export interface Body {
+  code: string;
+  address: string;
+  key: string;
+}
 
 const handler = nc()
   // use connect based middleware
   .use(cors())
-  .post(async (req, res) => {
-      try{
+  .post<{ body: Body }>(async (req, res) => {
+    try {
+      const { code, address, key } = req.body;
 
-          const { code, address, key } = req.body
+      const params = cbor.encode([
+        new Uint8Array(Buffer.from(code, "base64").buffer),
+      ]);
+      const sentTx = await install(address, key, params);
 
-          const params = cbor.encode([ new Uint8Array(Buffer.from(code, "base64").buffer) ])
-          const sentTx = await install(address, key, params)
+      if (sentTx.result.Receipt.ExitCode == 0) {
+        const respBuffer = Buffer.from(sentTx.result.Receipt.Return, "base64");
 
-          if(sentTx.result.Receipt.ExitCode == 0){
-              const respBuffer = Buffer.from(sentTx.result.Receipt.Return, "base64")
+        let arrayBuffer = new ArrayBuffer(respBuffer.length);
+        let typedArray = new Uint8Array(arrayBuffer);
+        for (let i = 0; i < respBuffer.length; ++i) {
+          typedArray[i] = respBuffer[i];
+        }
 
-              let arrayBuffer = new ArrayBuffer(respBuffer.length);
-              let typedArray = new Uint8Array(arrayBuffer);
-              for (let i = 0; i < respBuffer.length; ++i) {
-                  typedArray[i] = respBuffer[i];
-              }
+        const [cid, isInstalled] = cbor.decode(typedArray);
 
-              const [cid, isInstalled] = cbor.decode(typedArray)
-
-              res.statusCode = 200
-              res.end(JSON.stringify({cid: cid.toString(), installed: isInstalled}))
-          }
-      }catch(err: any){
-          res.statusCode = 400
-          res.end(JSON.stringify({errorMessage: err.toString()}))
+        res.statusCode = 200;
+        res.end(
+          JSON.stringify({ cid: cid.toString(), installed: isInstalled })
+        );
       }
-  })
+    } catch (err: any) {
+      res.statusCode = 400;
+      res.end(JSON.stringify({ errorMessage: err.toString() }));
+    }
+  });
 
 export default handler;
