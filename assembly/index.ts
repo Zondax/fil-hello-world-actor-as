c8ffd115b37efa7a79d4e0904cfa2d727818285d
Emmanuel <emmanuelm41@gmail.com>
2022-07-01 14:15:40 -0300

    make use of new packages to decode params automatically

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,27 +1,26 @@
 // @chainfile-index
 import {State} from "./state";
-import {ParamsRawResult} from "@zondax/fvm-as-sdk/assembly/env";
 
 // @ts-ignore
 @constructor
-function init(params:ParamsRawResult): void {
+function init(): void {
   // If we want to attach some storage to the instance,
   // a state needs to be saved at this step.
   // So we create a store, and call save method
-// @ts-ignore
+  // @ts-ignore
   State.defaultState().save()
 }
 
 // @ts-ignore
 @export_method(2)
 // User function. Smart-contract-related function.
-function say_hello(params:ParamsRawResult): string {
+function say_hello(): string {
   // If we want to restore the storage related to this instance,
   // we should call static load function. It will return a preloaded
   // state
 
-// @ts-ignore
-  const state = State.load();
+  // @ts-ignore
+  const state = State.load() as State;
 
   // Do some stuff with the state
   state.count += 1;
